9bad98c193fb23eec303be35b093d46a728c6647
kevin1010607 <kevin.1010607@gmail.com>
2023-08-09 21:36:09 +0800

    update 08/09:21

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def root():
     name = os.environ.get('SERVICE_NAME', 'Unknown')
     exec_time = int(os.environ.get('EXEC_TIME', 5))
     time.sleep(exec_time)
-    return random.random()
+    return f'{random.random()}\n'
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0',port=int(os.environ.get('PORT', 8080)))
